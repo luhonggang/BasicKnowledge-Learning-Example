f2f81e0dba9fb0c30e2138a113247b0e81fa587f
luhonggang <lhtarena@163.com>
2018-05-22 17:51:50 +0800

    study jdkproxy and cglibproxy with `程序员DD` project update

--- chapter-proxy-01/src/test/java/com/didispace/ApplicationTests.java
+++ chapter-proxy-01/src/test/java/com/didispace/ApplicationTests.java
@@ -3,12 +3,14 @@ package com.didispace;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
 public class ApplicationTests {
 
 	private Logger logger = Logger.getLogger(getClass());
