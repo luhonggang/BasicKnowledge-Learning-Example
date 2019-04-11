55a08325d3d550c545b31b4afdfc1c38b5873a3d
luhonggang <XIAOniu666>
2019-04-11 17:09:59 +0800

    Java中只存在值传递~~~~~~,基本类型值传递测试

--- chapter-java8-01/src/main/java/com/study/param/ParamTest.java
+++ chapter-java8-01/src/main/java/com/study/param/ParamTest.java
@@ -48,16 +48,25 @@ public class ParamTest {
         System.out.println("输出的结果03 : " + test);
         // 最终发现 :  test引用变量对应的对象的值 并没有被改变
 
+        int i = 200;
+        System.out.println(" 涉世未深,原本的样貌 : " + i);
+        changeBasicType(i);
+        System.out.println(" 历经沧桑,原本的模样 : " + i);
     }
 
     /**
      * 查看是否改变原始对象的 值
      * @param user
      */
-    public void change(ParamTest user){
+    private void change(ParamTest user){
         user = new ParamTest();
         user.setName("no-hello");
         user.setGender("女");
         System.out.println("输出的结果02 : " + user);
+    }
+
+    private static  void changeBasicType(int i){
+        i = 100;
+        System.out.println(" 正在经历着现实社会骨感的刺激,样貌开始变化 : " + i);
     }
 }
