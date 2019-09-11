152cf00eaf99f133a7fbb68a1a9b2b9681e0f0fa
luhonggang <XIAOniu666>
2019-09-11 17:44:55 +0800

    策略模式spring中集成简单了解

--- chapter-designmode-01/src/main/java/com/example/strategicmodel/difficult/service/PaymentProcessor.java
+++ chapter-designmode-01/src/main/java/com/example/strategicmodel/difficult/service/PaymentProcessor.java
@@ -2,13 +2,17 @@ package com.example.strategicmodel.difficult.service;
 
 import com.example.annotation.HandlerType;
 import com.example.strategicmodel.difficult.HandlerContext;
+import com.example.strategicmodel.difficult.PaymentDto;
 import com.example.utils.ClassScanner;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -17,9 +21,9 @@ import java.util.concurrent.ConcurrentHashMap;
  * @date 2019/9/10 15:40
  */
 @Component
-public class PaymentProcessor /*extends DefaultListableBeanFactory */implements BeanFactoryPostProcessor {
+public class PaymentProcessor /*extends DefaultListableBeanFactory */ implements BeanFactoryPostProcessor, BeanPostProcessor {
 
-    private static final String CURRENT_PACKAGE_PATH="com.example.strategicmodel.difficult.service.impl";
+    private static final String CURRENT_PACKAGE_PATH = "com.example.strategicmodel.difficult.service.impl";
 
    /* @Override
     public Map<String, Object> getBeansWithAnnotation(Class<? extends Annotation> annotationType) {
@@ -36,19 +40,70 @@ public class PaymentProcessor /*extends DefaultListableBeanFactory */implements 
         return results;
     }*/
 
+    public Map<String, PaymentBusiness> cacheMaps = new ConcurrentHashMap<>(3);
+
+    private PaymentProcessor() {
+    }
+
+    public PaymentProcessor(Map<String, PaymentBusiness> cacheMaps) {
+        this.cacheMaps = cacheMaps;
+    }
+
+    /**
+     * 2个处理器是 spring 对bean 进行生命周期管理的扩展,整个bean 生命周期中均会执行该这2个处理器
+     * spring bean 前置处理器
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (Objects.nonNull(bean) && bean instanceof PaymentBusiness) {
+            String type = bean.getClass().getAnnotation(HandlerType.class).value();
+            if (StringUtils.hasLength(type)) {
+                cacheMaps.put(type, ((PaymentBusiness) bean));
+            }
+        }
+        return bean;
+    }
+
+    /**
+     * spring bean 后置处理器
+     *
+     * @param bean
+     * @param beanName
+     * @return
+     * @throws BeansException
+     */
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        // 只有当前实例是PaymentBusiness类型的实例的时候才做控制台输出
+        if (Objects.nonNull(bean) && bean instanceof PaymentBusiness) {
+            for (Map.Entry<String, PaymentBusiness> kvEntry : cacheMaps.entrySet()) {
+                PaymentDto paymentDto = new PaymentDto();
+                paymentDto.setPayType("1");
+                System.out.println("key : " + kvEntry.getKey() + "value : " + kvEntry.getValue().payWay(paymentDto));
+            }
+        }
+        return bean;
+    }
+
     /**
      * 注册bean
+     *
      * @param beanFactory
      * @throws BeansException
      */
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        Map<String,Class> handlerMaps = new ConcurrentHashMap<>(3);
-        ClassScanner.scan(CURRENT_PACKAGE_PATH, HandlerType.class).forEach(clazz->{
+        Map<String, Class> handlerMaps = new ConcurrentHashMap<>(3);
+        ClassScanner.scan(CURRENT_PACKAGE_PATH, HandlerType.class).forEach(clazz -> {
             String type = clazz.getAnnotation(HandlerType.class).value();
-            handlerMaps.put(type,clazz);
+            handlerMaps.put(type, clazz);
         });
         HandlerContext context = new HandlerContext(handlerMaps);
-        beanFactory.registerSingleton(HandlerContext.class.getName(),context);
+        beanFactory.registerSingleton(HandlerContext.class.getName(), context);
     }
 }
--- chapter-designmode-01/src/main/java/com/example/web/HelloWorldController.java
+++ chapter-designmode-01/src/main/java/com/example/web/HelloWorldController.java
@@ -2,8 +2,9 @@ package com.example.web;
 
 import com.example.strategicmodel.difficult.HandlerContext;
 import com.example.strategicmodel.difficult.PaymentDto;
+import com.example.strategicmodel.difficult.service.PaymentProcessor;
+import jdk.nashorn.internal.ir.annotations.Reference;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,12 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HelloWorldController {
 
-    @Autowired
+    @Reference
     HandlerContext handlerContext;
 
+    @Reference
+    PaymentProcessor processor;
+
+    /**
+     * 使用策略模式 2 种注入bean的方式
+     *
+     * @param paymentDto 入参
+     * @return String
+     * @throws ClassNotFoundException
+     */
     @GetMapping("/test")
     public String getInstance(PaymentDto paymentDto) throws ClassNotFoundException {
         log.info("支付渠道选择入参 : {} ",paymentDto);
         return handlerContext.getInstance(paymentDto.getPayType()).payWay(paymentDto);
+//        return processor.cacheMaps.get(paymentDto.getPayType()).payWay(paymentDto);
     }
 }
